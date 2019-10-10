b25d8bd10d5ac8d46f3c5fc847cd54e9bd285959
Mehmet.Yuce <mehmet.yuce@logosgt.com.tr>
2019-10-10 18:20:23 +0300

    seeing connection

--- Windows/scratch-link/ArduinoSession.cs
+++ Windows/scratch-link/ArduinoSession.cs
@@ -2,50 +2,93 @@ using Fleck;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.IO.Ports;
+using System.Management;
 using System.Threading.Tasks;
-using Windows.Devices.Bluetooth;
-using Windows.Devices.Bluetooth.Rfcomm;
-using Windows.Devices.Enumeration;
-using Windows.Networking.Sockets;
-using Windows.Storage.Streams;
+using System.Linq;
 
 namespace scratch_link
 {
     internal class ArduinoSession : Session
     {
+        private Dictionary<string, dynamic> listOfDevices =
+                                new Dictionary<string, dynamic>();
         public ArduinoSession(IWebSocketConnection webSocket, int bufferSize = 4096, int maxMessageSize = 1048576) : base(webSocket, bufferSize, maxMessageSize)
         {
         }
-
-        public override bool Equals(object obj)
+        private async void GetListOfDevices()
         {
-            return base.Equals(obj);
-        }
+            await Task.Run(() =>
+            {
+                string[] portNames = SerialPort.GetPortNames();// Get real serial ports
+                foreach (string portName in portNames)
+                {
 
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
+                    List<string> otherSerial = new List<string>();
+                    List<ManagementObject> listObj = new List<ManagementObject>();//using System.Management;
+                    try
+                    {
+                        // get only devices that are working properly."
+                        string query = "SELECT * FROM Win32_PnPEntity WHERE ConfigManagerErrorCode = 0 AND NAME LIKE '%(" + portName + "%'";// get only devices that are working properly."
+                        ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+                        listObj = searcher.Get().Cast<ManagementObject>().ToList();
+                        searcher.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        otherSerial = new List<string>(); // clear the list because there are errors
+                                                          // addcode here to handle the error.....
+                    }
+                    foreach (ManagementObject obj in listObj)
+                    {
+                        //System.Console.WriteLine(obj["Availability'"]);
+                        System.Console.WriteLine(obj["Caption"]);
+                        System.Console.WriteLine(obj["ClassGuid"]);
+                        System.Console.WriteLine(obj["CompatibleID"]);
+                        System.Console.WriteLine(obj["ConfigManagerErrorCode"]);
+                        System.Console.WriteLine(obj["ConfigManagerUserConfig"]);
+                        System.Console.WriteLine(obj["CreationClassName"]);
+                        System.Console.WriteLine(obj["Description"]);
+                        String deviceID = (String)obj["DeviceID"];
+                        System.Console.WriteLine(deviceID);
+                        System.Console.WriteLine(obj["ErrorCleared"]);
+                        System.Console.WriteLine(obj["ErrorDescription"]);
+                        System.Console.WriteLine(obj["HardwareID"]);
+                        System.Console.WriteLine(obj["InstallDate"]);
+                        System.Console.WriteLine(obj["LastErrorCode"]);
+                        System.Console.WriteLine(obj["Manufacturer"]);
+                        System.Console.WriteLine(obj["Name"]);
+                        System.Console.WriteLine(obj["PNPClass"]);
+                        System.Console.WriteLine(obj["PNPDeviceID"]);
+                        //uint16 PowerManagementCapabilities[]"]);
+                        //boolean PowerManagementSupported"]);
+                        System.Console.WriteLine(obj["Present"]);
+                        System.Console.WriteLine(obj["Service"]);
+                        System.Console.WriteLine(obj["Status"]);
+                        System.Console.WriteLine(obj["StatusInfo"]);
+                        System.Console.WriteLine(obj["SystemCreationClassName"]);
+                        System.Console.WriteLine(obj["SystemName"]);
+                        object captionObj = obj["Caption"]; //This will get you the friendly name.
+                        this.listOfDevices.Add(portName, obj);
+                        var peripheralData = new JObject
+                        {
+                            new JProperty("name", new JValue(captionObj)),
+                            new JProperty("rssi", null),
+                            new JProperty("peripheralId", new JValue(deviceID))
+                        };
+                        SendRemoteRequest("didDiscoverPeripheral", peripheralData);
+                    }
+                }
+            });
         }
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
-
         protected override async Task DidReceiveCall(string method, JObject parameters, Func<JToken, JsonRpcException, Task> completion)
         {
             switch (method)
             {
-                case "discover":
-                    var peripheralData = new JObject
-                    {
-                        new JProperty("name", new JValue("deneme")),
-                        new JProperty("rssi", new JValue("127")),
-                        new JProperty("peripheralId", new JValue("123"))
-                    };
-                    SendRemoteRequest("didDiscoverPeripheral", peripheralData);
-                    //await completion("didDiscoverPeripheral", peripheralData);
+                case "discover": 
+                    GetListOfDevices();
+                    await completion(null, null);
                     break;
                 case "connect":
                     await completion(null, null);
@@ -54,7 +97,7 @@ namespace scratch_link
                     await completion(null, null);
                     break;
                 case "read":
-                    await completion("test", null);
+                    await completion(new JValue("test"), null);
                     break;
                 case "startNotifications":
                     await completion(null, null);
@@ -81,5 +124,50 @@ namespace scratch_link
         {
             return base.GetVersion();
         }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    internal class Win32_PnPEntity
+    {
+        UInt16 Availability;
+        string Caption;
+        string ClassGuid;
+        string[] CompatibleID;
+        UInt32 ConfigManagerErrorCode;
+        bool ConfigManagerUserConfig;
+        string CreationClassName;
+        string Description;
+        string DeviceID;
+        bool ErrorCleared;
+        string ErrorDescription;
+        string[] HardwareID;
+        DateTime InstallDate;
+        UInt32 LastErrorCode;
+        string Manufacturer;
+        string Name;
+        string PNPClass;
+        string PNPDeviceID;
+        UInt16[] PowerManagementCapabilities;
+        bool PowerManagementSupported;
+        bool Present;
+        string Service;
+        string Status;
+        UInt16 StatusInfo;
+        string SystemCreationClassName;
+        string SystemName;
     }
 }
